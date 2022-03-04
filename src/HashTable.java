ae59ea21c00a1d10b8b7eeebe904a0b98f67a9a4
apenny23 <apenny23@kentdenver.org>
2022-03-04 10:00:52 -0700

    Added the put method as well as the private helper methods hash and keyToInt

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,7 +1,7 @@
 public class HashTable {
     private String[] table;
     private int key;
-    private int size=0;
+    private int numVals=0;
     private double loadFactor = .75;
 
     public HashTable(int capacity){
@@ -16,18 +16,31 @@ public class HashTable {
         return ret;
     }
 
-    private int hash(int val, int key){
-        
+    private int hash(double val, int key){
+        double inp=Math.pow(val,3.0);
+        int output=(int)inp % key;
+        return output;
     }
 
     public boolean put(String key, String value){
-
-
+        if((double)table.length/(double)numVals>=.75){
+            String[] temp=new String[table.length*2];
+            for(int i=0;i<numVals;i++){
+                temp[i]=table[i];
+            }
+            table=temp;
+        }
+        int index=hash((double)keyToInt(value), keyToInt(key));
+        if(table[index].isEmpty()==false){
+            return false;
+        }
+        table[index]=value;
+        return true;
     }
 
     public static void main(String[] args) {
         HashTable test=new HashTable(99);
-        System.out.println(test.keyToInt("test"));
+        System.out.println(test.hash(992.0,103));
     }
 
 }
