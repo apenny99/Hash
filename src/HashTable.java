c06ea92c3403f288f3b6c27f40709c07e31e311d
apenny23 <apenny23@kentdenver.org>
2022-03-08 11:04:37 -0700

    Added comments

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,49 +1,55 @@
+/**
+ * Hash
+ * Given hash string key and value, stores it in a string array at a calculated index
+ * Author: August Penny
+ * Collaborator(s): Mareks
+ * Collaboration: Talked in class about ways to generate a large number
+ * Date: 3/8/22
+ **/
+
 public class HashTable {
     private String[] table;
-    private int key;
-    private int numVals=0;
-    private double loadFactor = .75;
 
     public HashTable(int capacity){
-        table=new String[capacity];
+        table=new String[capacity]; //makes new table with given capacity. constructor
     }
 
-    private int hash(String inp){
+    private int hash(String inp){ //generates unique int based on a string input
         long fin=0;
         String end="";
-       for(int i=0;i<inp.length();i++){
+       for(int i=0;i<inp.length();i++){//makes a string out of the int values of each char concattenated
            Integer temp=(int)inp.charAt(i);
            temp=temp%31;
            end+=temp.toString();
        }
-       fin=Long.parseLong(end);
-       long ret=fin % 997;
+       fin=Long.parseLong(end);//makes the string into a long
+       long ret=fin % 997;//makes a long that is the previous long modded by a large prime number
 
-       return (int)ret;
+       return (int)ret;//returns the final long casted to int
     }
 
 
-    public boolean put(String key, String value){
+    public boolean put(String key, String value){ //puts value in aarray at calculated integer place
         int index=hash(key);
-        if(index<= table.length){
+        if(index<= table.length){//checks if the key has been used and returns null if it has
             if(table[index]!=null){
                 return false;
             }
         }
 
-        while(index>= table.length){
+        while(index>= table.length){ //doubles array length if the index it out of bounds
             String[] temp = new String[table.length*2];
             for(int i=0;i<table.length;i++){
                 temp[i]=table[i];
             }
             table=temp;
         }
-        table[index]=value;
+        table[index]=value; //sets the value at the index to the string value
         return true;
 
     }
 
-    public String get(String key){
+    public String get(String key){//returns the value associated with the key
         int index=hash(key);
         if(index>= table.length){
             System.out.println("null");
@@ -51,12 +57,6 @@ public class HashTable {
         }
         return table[index];
     }
-
-    public static void main(String[] args) {
-        HashTable test=new HashTable(999);
-        System.out.println(test.put("August","Mareks"));
-        System.out.println(test.put("August","Mareks"));
-        System.out.println(test.get("August"));
-    }
+    
 
 }
